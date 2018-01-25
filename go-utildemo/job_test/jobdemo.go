d7de9df64546489956ca2018d0d09809ee5947f1
panenming <panenming@enn.cn>
2018-01-25 17:05:32 +0800

    time out demo

--- go-utildemo/job_test/jobdemo.go
+++ go-utildemo/job_test/jobdemo.go
@@ -14,6 +14,12 @@ func main() {
 		fmt.Println("运行中。。。")
 	}, expr)
 
+	callBack := func() {
+		fmt.Println("超时")
+	}
+
+	jobs.SetTimeOut(10*time.Second, callBack)
+
 	jobs.Start()
 	time.Sleep(60 * time.Second)
 	// 使用for{}堵塞会导致cpu飘高，导致cpu一个核100% 建议使用select{}
